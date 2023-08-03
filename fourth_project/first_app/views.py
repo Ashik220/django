ec1694c36444690b123d86460cd51ba12c6a2ed1
Khandoker Ashikur Rahman <96872538+Ashik220@users.noreply.github.com>
2023-08-03 12:53:45 +0600

    Add files via upload

---
+++ fourth_project/first_app/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request, './first_app/home.html', {"name" : "I am Rahim", "marks" : 86,"courses" : [
+        {
+        "id" : 1,
+        "course" : "C",
+        "teacher" : "Rahim"
+        },
+        {
+        "id" : 2,
+        "course" : "C++",
+        "teacher" : "Kahim"
+        },
+        {
+        "id" : 3,
+        "course" : "Python",
+        "teacher" : "Fahim"
+        },
+        ]})
